392f28d1ca3593bf1baac3066daa5df95339b66c
Christophe Gragnic <christophegragnic@gmail.com>
2014-06-07 21:58:50 +0200

    Web IDE: bouton OK, nvelle console, suppr du correc orthographique.

--- web/ide_injections.js
+++ web/ide_injections.js
@@ -8,6 +8,11 @@ var emulisp_states = {};
 // The marvellous PicoLisp prompt:
 var malg_prompt = ": ";
 
+function isTouch() {
+    if ("ontouchstart" in window || navigator.msMaxTouchPoints) return true;
+    return false;
+}
+
 function cleanTransient(text) {
     text = text.replace(/\^J/g,'\n');  // PicoLisp control char
     text = text.replace(/\n$/,'');     // remove last newline
@@ -33,10 +38,16 @@ function stdPrint(text, state) {
             repl_elt.val(repl_elt.val() + "\n" + text);
         }
     }
+    if (state.context.type == 'jrepl') {
+        state.context.term.echo(text);
+    }
 }
 
 function stdPrompt() {
-    return window.prompt(EMULISP_CORE.eval('*LastStdOut').slice(1, -1));
+    var last_line_displayed = EMULISP_CORE.eval('*LastStdOut').slice(1, -1);
+    var user_input = window.prompt(last_line_displayed);
+    if (user_input !== null) return user_input;
+    else throw new Error("Opération 'Demander' annulée.")
 }
 
 function onCtrlEnter(elt, f) {
@@ -45,6 +56,29 @@ function onCtrlEnter(elt, f) {
             f(elt);
         }
     });
+}
+
+function ide_action(editor_elt) {
+    // Fetch the relevant state.
+    var elt_id = editor_elt.attr('id').slice(0, -('-malg-editor'.length));
+    EMULISP_CORE.init(emulisp_states[elt_id]);
+    // Process src.
+    var src = editor_elt.val();
+    // createRichInput put the editor in a sub div, that's why we use
+    // parent().parent()
+    var error_elt = editor_elt.parent().parent().find('.malg-error').first();
+    var display_elt = editor_elt.parent().parent().find('.malg-display').first();
+    display_elt.html('&nbsp;');
+    try {
+        EMULISP_CORE.eval(src).toString();
+        error_elt.text('');
+    } catch(e) {
+        error_elt.text(e.toString());
+    }
+    if (typeof(Storage) !== "undefined") {
+        var key = 'microalg_src_' + elt_id;
+        localStorage[key] = src;
+    }
 }
 
 function inject_microalg_editor_in(elt_id, config, msg) {
@@ -56,11 +90,12 @@ function inject_microalg_editor_in(elt_id, config, msg) {
     emulisp_states[elt_id] = EMULISP_CORE.currentState();
     // Build the html and bind to ide_action.
     var script_container = $('#' + elt_id);
-    var script_string = '<textarea id="' + elt_id + '-malg-editor" class="malg-editor" cols="80" rows="2" >' + msg + '</textarea>' +
+    var script_string = '<textarea id="' + elt_id + '-malg-editor" class="malg-editor" cols="80" rows="2" spellcheck="false">' + msg + '</textarea>' +
+            (isTouch()?'<input type="button" onclick="ide_action($(\'#' + elt_id + '-malg-editor\'))" value="OK" class="malg-ok"/>':'') +
             '<div class="malg-error" style="color: red;"></div>' +
             '<div id="' + display_target_id + '" class="malg-display">&nbsp;</div>';
     script_container.html(script_string);
-    var editor = script_container.find('.malg-editor').first();
+    var editor = $('#' + elt_id + '-malg-editor');
     // Load local storage in the editor.
     if (config.localStorage && typeof(Storage)!=="undefined") {
         var key = 'microalg_src_' + elt_id;
@@ -70,31 +105,35 @@ function inject_microalg_editor_in(elt_id, config, msg) {
     }
     createRichInput(editor);
     onCtrlEnter(editor, ide_action);
-    function ide_action(editor_elt) {
-        // Fetch the relevant state.
-        EMULISP_CORE.init(emulisp_states[editor_elt.attr('id').slice(0, -('-malg-editor'.length))]);
-        // Process src.
-        var src = editor_elt.val();
-        // createRichInput put the editor in a sub div, that's why we use
-        // parent().parent()
-        var error_elt = editor_elt.parent().parent().find('.malg-error').first();
-        var display_elt = editor_elt.parent().parent().find('.malg-display').first();
-        display_elt.html('&nbsp;');
-        try {
-            EMULISP_CORE.eval(src).toString();
-            error_elt.text('');
-        } catch(e) {
-            error_elt.text(e.toString());
-        }
-        if (config.localStorage && typeof(Storage) !== "undefined") {
-            localStorage[key] = src;
+}
+
+function repl_action(repl_elt) {
+    // Fetch the relevant state.
+    EMULISP_CORE.init(emulisp_states[repl_elt.attr('id')]);
+    var result;
+    var repl_content = repl_elt.val();
+    var src = repl_content.slice(EMULISP_CORE.currentState().old_src.length,
+                                 repl_content.length);
+    try {
+        result = EMULISP_CORE.eval(src);
+    } catch(e) {
+        if (e.toString() == "Error: Function 'bye' not supported") {
+            // Destroy
+            repl_container.html('');
+        } else {
+            repl_elt.val(repl_elt.val() + "\n" + e.toString());
         }
     }
+    if (result != '""') {
+        repl_elt.val(repl_elt.val() + "\n-> " + cleanTransient(result));
+    }
+    repl_elt.val(repl_elt.val() + "\n" + malg_prompt);
+    EMULISP_CORE.currentState().old_src = repl_elt.val();
 }
 
 function inject_microalg_repl_in(elt_id, msg) {
     // Custom state for a custom display in the REPL.
-    var repl_id = elt_id + '-mals-repl';
+    var repl_id = elt_id + '-malg-repl';
     var state_clone = jQuery.extend(true, {}, microalg_fresh_state);
     EMULISP_CORE.init(state_clone);
     EMULISP_CORE.currentState().context = {type: 'repl', display_elt: repl_id};
@@ -102,37 +141,57 @@ function inject_microalg_repl_in(elt_id, msg) {
     // Build the html and bind to ide_action.
     var repl_container = $('#' + elt_id);
     var rows = msg.split('\n').length;
-    var repl_string = '<textarea id="' + repl_id + '" class="malg-repl" rows="' + (rows+2) + '" >' + malg_prompt + msg + '</textarea>';
+    var repl_string = '<textarea id="' + repl_id + '" class="malg-repl" rows="' + (rows+2) + '" spellcheck="false">' + malg_prompt + msg + '</textarea>' +
+        (isTouch()?'<input type="button" onclick="repl_action($(\'#' + repl_id +'\'))" value="OK" class="malg-ok"/>':'');
     repl_container.html(repl_string);
     var repl = $('#' + repl_id);
     createRichInput(repl);
     onCtrlEnter(repl, repl_action);
-    var old_src = malg_prompt;
-    function repl_action(repl_elt) {
-        // Fetch the relevant state.
-        EMULISP_CORE.init(emulisp_states[repl_elt.attr('id')]);
-        var result;
-        var repl_content = repl_elt.val();
-        var src = repl_content.slice(old_src.length, repl_content.length);
-        try {
-            result = EMULISP_CORE.eval(src);
-        } catch(e) {
-            if (e.toString() == "Error: Function 'bye' not supported") {
-                // Destroy
-                repl_container.html('');
-            } else {
-                repl_elt.val(repl_elt.val() + "\n" + e.toString());
-            }
-        }
-        if (result != EMULISP_CORE.NIL) {
-            repl_elt.val(repl_elt.val() + "\n-> " + cleanTransient(result));
-        }
-        repl_elt.val(repl_elt.val() + "\n" + malg_prompt);
-        old_src = repl_elt.val();
-    }
+    EMULISP_CORE.currentState().old_src = malg_prompt;
 }
 
-http://www.sitepoint.com/jquery-set-focus-character-range/
+function inject_microalg_jrepl_in(elt_id, msg) {
+    $('#' + elt_id).terminal(function(command, term) {
+        if (command !== '') {
+            // Fetch the relevant state.
+            EMULISP_CORE.init(emulisp_states[elt_id]);
+            try {
+                var result = EMULISP_CORE.eval(command);
+                if (result != '""') {
+                    term.echo('-> ' + cleanTransient(result.toString()));
+                }
+            } catch(e) {
+                if (e.toString() == "Error: Function 'bye' not supported") {
+                    term.destroy();
+                } else {
+                    term.error(new String(e));
+                }
+            }
+        }
+    }, {
+        greetings: msg,
+        name: 'malg_repl',
+        height: 150,
+        prompt: ': ',
+        clear: false,
+        exit: false,
+        keypress: function(e, term) {
+            // http://stackoverflow.com/questions/23817604/how-to-hook-on-keypress-and-grab-the-current-content-of-the-terminal
+            setTimeout(function() {
+                if (false) console.log(term.html());
+            }, 5);
+        },
+        onInit: function(term) {
+            // Custom state for a custom display in the REPL.
+            var state_clone = jQuery.extend(true, {}, microalg_fresh_state);
+            EMULISP_CORE.init(state_clone);
+            EMULISP_CORE.currentState().context = {type: 'jrepl', term: term};
+            emulisp_states[elt_id] = EMULISP_CORE.currentState();
+        }
+    });
+}
+
+// http://www.sitepoint.com/jquery-set-focus-character-range/
 $.fn.selectRange = function(start, end) {
     return this.each(function() {
         if (this.setSelectionRange) {
